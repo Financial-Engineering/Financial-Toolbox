3bfbceb5f781f865cea9d525e63f8956914f5654
Richard Lewis <rlewis@iMac.local>
2011-03-13 19:06:18 +1100

    Initial Commit

---
+++ Financial ToolboxTests/Financial_ToolboxTests.h
@@ -0,0 +1,17 @@
+//
+//  Financial_ToolboxTests.h
+//  Financial ToolboxTests
+//
+//  Created by Richard Lewis on 3/13/11.
+//  Copyright 2011 RRT. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+
+@interface Financial_ToolboxTests : SenTestCase {
+@private
+    
+}
+
+@end
---
+++ Financial ToolboxTests/Financial_ToolboxTests.m
@@ -0,0 +1,33 @@
+//
+//  Financial_ToolboxTests.m
+//  Financial ToolboxTests
+//
+//  Created by Richard Lewis on 3/13/11.
+//  Copyright 2011 RRT. All rights reserved.
+//
+
+#import "Financial_ToolboxTests.h"
+
+
+@implementation Financial_ToolboxTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in Financial ToolboxTests");
+}
+
+@end
